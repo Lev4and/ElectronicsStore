f134c06a78e5139465edbd9a03e28bd7668c5bdf
Lev4and <54065747+Lev4and@users.noreply.github.com>
2020-12-20 11:07:42 +0500

    Изменение таблицы продуктов в корзине
    Таблица продуктов в корзине стала максимально широкой. Можно отдельно просматривать информацию о продукте в корзине. Изменение стилей.

--- Views/Renders/TableProductsInBasket.php
+++ Views/Renders/TableProductsInBasket.php
@@ -1,5 +1,5 @@
 <div class="table-block">
-    <table border="1">
+    <table border="1" style="width: 100%">
         <tr class="table-block-headers">
             <th class="table-block-header">Выбрать</th>
             <th class="table-block-header">Фото</th>
@@ -12,9 +12,9 @@
             <tr id="<?php echo $product["id"]; ?>" class="table-block-values">
                 <td class="table-block-value-input-radio"><input type="hidden" name="basket[<?php echo $product["id"]; ?>][productId]" value="<?php echo $product["id"]; ?>"><input type="radio" name="selectedProduct" checked="checked" value="<?php echo $product["id"]; ?>"></td>
                 <td class="table-block-value-image"><div><img src="/Resources/Images/Upload/<?php echo $product["photo"]; ?>"></div></td>
-                <td class="table-block-value-text"><?php echo "{$product["manufacturer_name"]} {$product["model"]}"; ?></td>
+                <td class="table-block-value-text"><a href="<?php echo "http://{$_SERVER["SERVER_NAME"]}/Views/Pages/Customer/Catalog/Product.php?productId={$product["id"]}" ?>" style="text-decoration: none; color: black"><?php echo "{$product["manufacturer_name"]} {$product["model"]}"; ?></a></td>
                 <td class="table-block-value-input-number"><input type="number" name="basket[<?php echo $product["id"]; ?>][number]" min="1" onchange="numberItemsChanged();" value="1"></td>
-                <td class="table-block-value-text"><input type="hidden" name="basket[<?php echo $product["id"]; ?>][price]" value="<?php echo "{$product["price"]}"; ?>"><?php echo "{$product["price"]} ₽"; ?></td>
+                <td class="table-block-value-text" style="text-align: center"><input type="hidden" name="basket[<?php echo $product["id"]; ?>][price]" value="<?php echo "{$product["price"]}"; ?>"><?php echo "{$product["price"]} ₽"; ?></td>
             </tr>
         <? endforeach; ?>
     </table>
